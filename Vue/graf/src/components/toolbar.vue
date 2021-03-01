5ad414b6efa52c6fa587fd4aa3b624e0f68e7c9d
Michael Fitzgerald <fitzichael@gmail.com>
2021-02-28 22:18:41 -0500

    started working on a toolbar

---
+++ Vue/graf/src/components/toolbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="toolbar">
+    <button type="button">First</button>
+    <button type="button">Second</button>
+    <button type="button">Third</button>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'Toolbar',
+}
+</script>      
