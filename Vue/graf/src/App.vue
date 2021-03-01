e95e502273be703b6b89b7d9db734b0250d008ef
Nicholas <desman@rpi.edu>
2021-02-28 22:48:30 -0500

    Added Basic Graf Viewer
    Imported vue-d3-network and created simple graph

--- Vue/graf/src/App.vue
+++ Vue/graf/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Graf/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Graf from './components/Graf.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Graf
   }
 }
 </script>
---
+++ Vue/graf/src/components/Graf.vue
@@ -0,0 +1,60 @@
+<template>
+  <D3Network :net-nodes="nodes" :net-links="links" :options="options"/>
+</template>
+
+<script>
+import D3Network from 'vue-d3-network';
+
+export default {
+  name: 'Graf',
+  props: {
+  },
+  components: {
+    D3Network
+  },
+  data () {
+    return {
+      nodes: [
+        { id: 1 },
+        { id: 2 },
+        { id: 3 },
+        { id: 4 },
+        { id: 5 },
+        { id: 6 },
+        { id: 7 },
+        { id: 8 },
+        { id: 9 }
+      ],
+      links: [
+        { sid: 1, tid: 2, _color: 'black'},
+        { sid: 2, tid: 8, _color: 'black'},
+        { sid: 3, tid: 4, _color: 'black'},
+        { sid: 4, tid: 5, _color: 'black'},
+        { sid: 5, tid: 6, _color: 'black'},
+        { sid: 7, tid: 8, _color: 'black'},
+        { sid: 5, tid: 8, _color: 'black'},
+        { sid: 3, tid: 8, _color: 'black'},
+        { sid: 7, tid: 9, _color: 'black'}
+      ],
+      nodeSize:20,
+      canvas:false
+    };
+  },
+  computed:{
+    options(){
+      return{
+        force: 3000,
+        size:{ w:600, h:600},
+        nodeSize: this.nodeSize,
+        nodeLabels: true,
+        linkLabels:true,
+        canvas: this.canvas
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
