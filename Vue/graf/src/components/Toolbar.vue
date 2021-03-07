50dd99b368c510a11e296bf47fb1d308c31d0abf
Michael Fitzgerald <fitzichael@gmail.com>
2021-03-07 18:13:37 -0500

    made very basic toolbar

---
+++ Vue/graf/src/components/Toolbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="toolbar">
+    <button v-for="tool in tools"
+            v-bind:key="tool"
+            v-bind:class="{active: activeTool === tool}"
+            v-on:click="activeTool = tool">
+      {{tool}}
+    </button>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'Toolbar',
+  data: 
+  function() {
+    return {
+      activeTool: "node",
+      tools: ["node", "line", "text", "erase"]
+    }
+  }
+}
+</script>      
+
+<style scoped>
+button {
+  display: inline-block;
+  margin: 5px;
+  font-weight: normal;
+}
+.active {
+  font-weight: bold;
+}
+
+</style>
