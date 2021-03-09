3766b93ce61849c6392f7fa104dd0120cea0970b
Michael Fitzgerald <fitzichael@gmail.com>
2021-03-09 13:56:06 -0500

    toolbar is now draggable

--- Vue/graf/src/components/Toolbar.vue
+++ Vue/graf/src/components/Toolbar.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="toolbar">
+  <div id="toolbar">
+    <div id="toolbarheader"></div>
     <button v-for="tool in tools"
             v-bind:key="tool"
             v-bind:class="{active: activeTool === tool}"
@@ -40,6 +41,50 @@ export default{
           break;
       }
     });
+    
+    //theres probably a way to do this with more vue stuff, but I'm not sure what it is.
+    dragElement(document.getElementById("toolbar"));
+
+    function dragElement(elmnt) {
+      var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+      if (document.getElementById(elmnt.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+      } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+      }
+
+      function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+      }
+
+      function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+      }
+
+      function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+    }
   },
   data: 
   function() {
@@ -74,9 +119,17 @@ button {
 .active {
   background-color: LightGray;
 }
-.toolbar {
+#toolbar {
   display: inline-block;
   border: solid;
+  position: absolute;
+}
+#toolbarheader {
+  padding: 10px;
+  cursor: move;
+  z-index: 10;
+  background-color: black;
+  color: gray;
 }
 img {
   width: 20px;
