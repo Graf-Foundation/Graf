b0f2bf2481dfa792b29837db228eb24297a7af88
Michael Fitzgerald <fitzichael@gmail.com>
2021-03-08 19:12:47 -0500

    added keyboard shortcuts

--- Vue/graf/src/components/Toolbar.vue
+++ Vue/graf/src/components/Toolbar.vue
@@ -4,7 +4,7 @@
             v-bind:key="tool"
             v-bind:class="{active: activeTool === tool}"
             v-on:click="activeTool = tool">
-      <img v-bind:src=icons[tool] />
+      <img v-bind:src=icons[tool] v-bind:title="altTexts[tool]"/>
     </button>
   </div>
 </template>
@@ -17,6 +17,30 @@ import label_logo from '../assets/label.svg';
 import erase_logo from '../assets/erase.svg';
 export default{
   name: 'Toolbar',
+  mounted() {
+    document.addEventListener("keypress", (event) => {
+      //may want to consider making a class for scalability when we begin to add more features
+      switch(event.key){
+        case "s":
+          this.$set(this,"activeTool","Select");
+          break;
+        case "n":
+          this.$set(this,"activeTool","Node");
+          break;
+        case "e": 
+          this.$set(this,"activeTool","Edge");
+          break;
+        case "l":
+          this.$set(this,"activeTool","Label");
+          break;
+        case "r":
+          this.$set(this,"activeTool","Erase");
+          break;
+        default:
+          break;
+      }
+    });
+  },
   data: 
   function() {
     return {
@@ -27,7 +51,14 @@ export default{
         "Node" : node_logo,
         "Edge" : edge_logo,
         "Label" : label_logo,
-        "Erase" : erase_logo,
+        "Erase" : erase_logo
+      },
+      altTexts: {
+        "Select" : "(s)elect",
+        "Node" : "(n)ode",
+        "Edge" : "(e)dge",
+        "Label" : "(l)abel",
+        "Erase" : "e(r)ase"
       }
     }
   }
