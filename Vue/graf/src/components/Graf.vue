faac14f8f6c0df52aa117d4c966c8dbd8cf16184
jcasente <jacasente@gmail.com>
2021-03-10 20:04:37 -0500

    Update Graf.vue

--- Vue/graf/src/components/Graf.vue
+++ Vue/graf/src/components/Graf.vue
@@ -84,7 +84,7 @@ export default {
         size:{ w: window.innerWidth, h: window.innerHeight - 200},
         nodeSize: this.nodeSize,
         nodeLabels: true,
-        linkLabels:true,
+        linkSize: this.linkSize,
         canvas: this.canvas,
         linkWidth: 3,
         fontSize: 20
@@ -112,7 +112,7 @@ export default {
     change_node_label() {
       this.nodelabeler = false;
       this.nodes[this.selected].name = this.newlabel;
-      this.newlabel = ""
+      this.newlabel = "";
       this.selected = -1;
     },
     enable_edge_label(event,edge) {
@@ -123,8 +123,15 @@ export default {
     change_edge_label() {
       this.edgelabeler = false;
       this.links[this.selected].name = this.newlabel;
-      this.newlabel = ""
+      this.newlabel = "";
       this.selected = -1;
+
+      var t = this.nodes[0].name;
+      this.nodes[0].name = "TEMP";
+      this.nodes[0].name = t;
+      
+
+
     }
   }
 }
