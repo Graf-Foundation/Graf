de3eb3234e2de5afd934a151d56cee157d8fef1e
maxstewartsteele <66533762+maxstewartsteele@users.noreply.github.com>
2021-03-17 14:47:13 -0400

    Update main.js

--- Vue/graf/src/main.js
+++ Vue/graf/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import SuiVue from 'semantic-ui-vue';
+import router from './router';
 
 Vue.config.productionTip = false
+
 Vue.use(SuiVue);
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
