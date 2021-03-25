19fae6050a39848853c0b753e335573616e6b9c1
maxstewartsteele <66533762+maxstewartsteele@users.noreply.github.com>
2021-03-25 17:00:55 -0400

    Create AboutV2.vue

---
+++ Vue/graf/src/components/AboutV2.vue
@@ -0,0 +1,160 @@
+<template>
+  <div>
+    <div class="about-section">
+      <h1>About Us Page</h1>
+      <p>We Make Graf</p>
+    </div>
+    <h2 style="text-align:center">Developers</h2>
+      
+      
+    <div class="ui six column grid">
+      <div class="column">
+        <div class="ui fluid card">
+          <div class="image">
+            <img src="../assets/Face.png" >
+          </div>
+          <div class="content">
+            <a class="header">James Weir</a>
+            <div class="meta">
+              <span class="date">Project Leader</span>
+            </div>
+            <div class="description">
+              Some info would be here I guess.
+            </div>
+          </div>
+          <div class="extra content">
+            <a href="mailto:weirj@rpi.edu">
+              <i class="at icon"></i>
+              weirj@rpi.edu
+            <br>
+            </a>
+            <a href="https://github.com/JPWeir">
+              <i class="github square icon"></i>
+              GitHub
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  <!--
+    <div class="row">
+      <div class="column">
+        <div class="card">
+          <img src="../assets/Face.png" alt="James" style="width:50%" />
+          <div class="container">
+            <h2 class="name">James Weir</h2>
+            <p class="title">Project Leader</p>
+            <p class="description">Some info would be here I guess.</p>
+            <p>weirj@rpi.edu</p>
+            <p><a class="button" href="mailto:weirj@rpi.edu">Contact</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="card">
+          <img src="../assets/Face.png" alt="Nic" style="width:50%" />
+          <div class="container">
+            <h2 class="name">Nicholas Desmarais</h2>
+            <p class="title">Dad</p>
+            <p class="description">Some info</p>
+            <p>desman@rpi.edu</p>
+            <p><a class="button" href="mailto:desman@rpi.edu">Contact</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="card">
+          <img src="../assets/Face.png" alt="Max" style="width:50%" />
+          <div class="container">
+            <h2 class="name">Max Herman</h2>
+            <p class="title">Node guy</p>
+            <p class="description">Info</p>
+            <p>hermam3@rpi.edu</p>
+            <p><a class="button" href="mailto:hermam3@rpi.edu">Contact</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="card">
+          <img src="../assets/Face.png" alt="Max" style="width:50%" />
+          <div class="container">
+            <h2 class="name">Max Stewart Steele</h2>
+            <p class="title">Made this page</p>
+            <p class="description">More info</p>
+            <p>stewam8@rpi.edu</p>
+            <p><a class="button" href="mailto:stewam8@rpi.edu">Contact</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="card">
+          <img src="../assets/Face.png" alt="Micheal" style="width:50%" />
+          <div class="container">
+            <h2 class="name">Michael Fitzgerald</h2>
+            <p class="title">Doesn't like design</p>
+            <p class="description">INFO</p>
+            <p>fitzgm6@rpi.edu</p>
+            <p><a class="button" href="mailto:fitzgm6@rpi.edu">Contact</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="card">
+          <img src="../assets/Face.png" alt="Joseph" style="width:50%" />
+          <div class="container">
+            <h2 class="name">Joseph Casente</h2>
+            <p class="title">Fell down the stairs</p>
+            <p class="description">INFO in text form</p>
+            <p>casenj@rpi.edu</p>
+            <p><a class="button" href="mailto:casenj@rpi.edu">Contact</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class = "row">
+      -->
+    <center>
+        <div>
+        <router-link to="/">Return to main site</router-link>
+        </div>
+    </center>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Aboutv2",
+};
+</script>
+
+<style scoped>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+.about-section {
+  padding: 50px;
+  text-align: center;
+  background-color: #3cb15f;
+  color: white;
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+</style>
+
