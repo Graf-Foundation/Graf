42939a081135dc6bc4cdf027d42705aa420853cc
maxstewartsteele <66533762+maxstewartsteele@users.noreply.github.com>
2021-03-29 18:27:57 -0400

    Update Toolbar.vue

--- Vue/graf/src/components/Toolbar.vue
+++ Vue/graf/src/components/Toolbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="experiemntal">
     <sui-menu style="width: max-content;">
-      <a
+      <!-- <a
         v-for="tool in tools"
         is="sui-menu-item"
         v-bind:key="tool"
@@ -11,7 +11,80 @@
       >
 				<sui-icon v-bind:name="icons[tool]"/>
         <br>{{altTexts[tool]}}
-			</a>
+			</a> -->
+      <a 
+        is="sui-menu-item"
+        v-bind:key='"Select"'
+        @click='onToolChange("Select")'
+        :active='activeTool=="Select"'
+        >
+        <i class = 'mouse pointer icon'></i>
+        <br>(S)elect
+      </a>
+      <a 
+        is="sui-menu-item"
+        v-bind:key='"Node"'
+        @click='onToolChange("Node")'
+        :active='activeTool=="Node"'
+        >
+        <i class = 'circle icon'></i>
+        <br>(N)ode
+      </a>
+      <a 
+        is="sui-menu-item"
+        v-bind:key='"Edge"'
+        @click='onToolChange("Edge")'
+        :active='activeTool=="Edge"'
+         class = "ui simple dropdown item">
+        <i class = 'window minimize icon'></i>
+        <br>(E)dge
+        <div class="menu">
+          <a class="item">Undirected</a>
+          <a class="item">Directed</a>
+        </div>
+      </a>
+      <a 
+        is="sui-menu-item"
+        v-bind:key='"Label"'
+        @click='onToolChange("Label")'
+        :active='activeTool=="Label"'
+        >
+        <i class = 'pencil icon'></i>
+        <br>(L)abel
+      </a>
+      <a 
+      is="sui-menu-item"
+        v-bind:key='"Erase"'
+        @click='onToolChange("Erase")'
+        :active='activeTool=="Erase"'
+        >
+        <i class = 'eraser icon'></i>
+        <br>E(r)ase
+      </a>
+      <a
+        v-bind:key='"Algorithm"'
+        @click='onToolChange("Algorithm")'
+        :active='activeTool=="Algorithm"'
+        class = "ui simple dropdown item">
+        <i class = 'code branch icon'></i>
+        <br>(A)lgorithm
+        <div class="menu">
+          <a  
+          @click="onAlgorithmChange('bfs')"
+          class="item"
+          >BFS search
+          </a>
+
+          <a 
+          @click="onAlgorithmChange('djikstra')"
+          class="item"
+          >DFS search
+          </a>
+          
+          <a class="item">Djikstra</a>
+          <a class="item">Kosaraju</a>
+        </div>
+      </a>
     </sui-menu>
 </div>
 </template>
