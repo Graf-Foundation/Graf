4801d6f2b72084147f204e2e7e1dcd39328ebc62
Michael Fitzgerald <fitzichael@gmail.com>
2021-04-01 20:31:06 -0400

    help menu complete

--- Vue/graf/src/components/Help.vue
+++ Vue/graf/src/components/Help.vue
@@ -3,19 +3,9 @@
     <sui-button @click='toggle' class="right floated icon" icon="help"></sui-button>
     <sui-modal v-model='open'>
       <sui-modal-header>Welcome to Graf!</sui-modal-header>
-      <sui-modal-content>
+      <sui-modal-content scrolling>
         <sui-header>Quick Start Guide</sui-header>
-        <img src="../assets/toolbar.png"/>
-        <p>
-          At the top of the page you'll see the toolbar. 
-          The default tool is the Select tool, which can be used to select the nodes and edges on the graph. 
-          Use the Node tool to add nodes to the graph, and the Edge tool to connect two Nodes.
-          The label tool can be used to label nodes and edges.
-          The erase tool is used to erase nodes and edges.
-          The algorithm tool is used to apply algorithms to the graph, such as Dijkstra's algorithm.
-          The edge tool can also be used to create directed or undirected edges.
-          Happy Grafing!
-        </p>
+        <div v-html="helpContent"></div>
       </sui-modal-content>
       <sui-modal-actions>
         <sui-button positive @click='toggle'>
@@ -32,13 +22,20 @@ export default {
   name: "Help",
   data () {
     return {
-      open: false
+      open: false,
+      helpContent: "<p>Something went wrong</p>"
     }
   },
   methods: {
     toggle() {
       this.open = !this.open
     }
+  },
+  created: function () {
+    this.$http.get('static/html/Help.html').then(function(response){
+      console.log(response);
+      this.helpContent = response.data;
+    });
   }
 }
 
