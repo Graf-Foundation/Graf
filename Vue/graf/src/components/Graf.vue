fc92e75cf036e99d2eddb7dbdde16c04bdb32659
jcasente <jacasente@gmail.com>
2021-04-05 17:08:54 -0400

    Update Graf.vue

--- Vue/graf/src/components/Graf.vue
+++ Vue/graf/src/components/Graf.vue
@@ -60,6 +60,7 @@ import Toolbar from '../components/Toolbar.vue'
 import GrafTools from '../middleware/grafTools.js'
 import PathTools from '../middleware/pathTools.js'
 import helperFunctions from '../middleware/helperFunctions';
+import cookieHelpers from "..middleware/cookieHelper.js";
 //import Help from "../components/Help.vue";
 //import About from 'About.vue'
 
@@ -73,6 +74,12 @@ export default {
   mounted () {
 			document.addEventListener("keyup", this.keyup_handler, false);
       window.addEventListener('resize', this.resize_handler, false);
+      //Loading in the graf from cookies
+      cookieHelpers.checkRepCookie();
+      if(!cookieHelpers.isC) {
+        var d = grafhelpers.loadGraf(cookieHelpers.getCookie("GrafData"));
+        this.graf = d;
+      }
   },
   data () {
     return {
@@ -126,15 +133,21 @@ export default {
         this.graf.links = [];
         this.grafData = "";
         this.graf.aggCount = 1;
+      //Modifying cookie
+      cookieHelpers.putCookie("GrafData", JSON.stringify(this.graf));
     },
     onAlgorithmChange(alg) {
         this.selection.selectedAlgorithm = alg;
     },
     onUndo() {
         this.graf = helperFunctions.updateHistory(this.graf, this.history, true);
+        //Modifying cookie
+        cookieHelpers.putCookie("GrafData", JSON.stringify(this.graf));
     },
     onRedo() {
         this.graf = helperFunctions.updateHistory(this.graf, this.history, false);
+        //Modifying cookie
+        cookieHelpers.putCookie("GrafData", JSON.stringify(this.graf));
     },
     useTool(tool) {
       var oldData = JSON.stringify(this.graf);
@@ -160,6 +173,8 @@ export default {
         default:
           break;
       }
+      //Modifying cookie
+      cookieHelpers.putCookie("GrafData", JSON.stringify(this.graf));
 
       var newData = JSON.stringify(this.graf);
       if(oldData != newData) {
