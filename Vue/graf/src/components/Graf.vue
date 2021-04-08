3e0a34936621e13671a2bf7c3c373c2ea723f810
maxstewartsteele <66533762+maxstewartsteele@users.noreply.github.com>
2021-04-08 13:47:43 -0400

    Update Graf.vue

--- Vue/graf/src/components/Graf.vue
+++ Vue/graf/src/components/Graf.vue
@@ -2,32 +2,45 @@
   <div class="graf">
     <center>
       <header class="fixedTC">
-        <Toolbar @tool-change="change_tool" @alg-change="onAlgorithmChange"></Toolbar>
+        <Toolbar
+          @tool-change="change_tool"
+          @alg-change="onAlgorithmChange"
+        ></Toolbar>
+        <sui-button @click="onUndo()" icon="undo" />
+        <sui-button @click="onRedo()" icon="redo" />
 
-        <sui-button @click="onUndo();" icon="undo" />
-        <sui-button @click="onRedo();" icon="redo" />
-
-        <div class="labeler"  v-if="currentTool=='Label' && selection.selectedLabel"
-         style="margin: 1em 0em 0em"
+        <div
+          class="labeler"
+          v-if="currentTool == 'Label' && selection.selectedLabel"
+          style="margin: 1em 0em 0em"
         >
           <sui-input v-model="selection.selectedLabel.name" @keypress.stop />
-          <br>
+          <br />
           Change Label
         </div>
       </header>
 
-      <sui-dropdown class="fixedTR icon" icon="settings" button pointing="top right" floating>
+      <sui-dropdown
+        class="fixedTR icon"
+        icon="settings"
+        button
+        pointing="top right"
+        floating
+      >
         <sui-dropdown-menu>
           <sui-dropdown-item>
             <router-link to="/about">About</router-link>
           </sui-dropdown-item>
-
-          <sui-dropdown-item> 
+          <sui-dropdown-item>
+            <sui-button class="ui button" @click.stop="visible = true">
+                Menu
+            </sui-button>
+          </sui-dropdown-item>
+          <sui-dropdown-item>
             <router-link to="/help">Help</router-link>
           </sui-dropdown-item>
         </sui-dropdown-menu>
       </sui-dropdown>
-
 
       <D3Network
         id="grafNet"
@@ -39,119 +52,132 @@
       />
 
       <div class="fixedBC graf-labeler">
-        <sui-button @click="onSaveImage();" color="green" content="Save Image"/>
-        <sui-button @click="onSaveGraf();" color="green" content="Save Graph"/>
-        <sui-button @click="onResetGraf();" color="green" content="Reset Graph"/>
-        <br>
-        <sui-input placeholder="Load Graf" v-model="grafData" @keyup.enter="onLoadGraf()"/>
+        <sui-button @click="onSaveImage()" color="green" content="Save Image" />
+        <sui-button @click="onSaveGraf()" color="green" content="Save Graph" />
+        <sui-button
+          @click="onResetGraf()"
+          color="green"
+          content="Reset Graph"
+        />
+        <br />
+        <sui-input
+          placeholder="Load Graf"
+          v-model="grafData"
+          @keyup.enter="onLoadGraf()"
+        />
       </div>
 
-      <Help class="fixedBR"/>
-
+      <Help class="fixedBR" />
     </center>
   </div>
 </template>
 
 <script>
-import D3Network from 'vue-d3-network';
-import grafhelpers from '../middleware/helperFunctions';
-import Toolbar from '../components/Toolbar.vue'
-import GrafTools from '../middleware/grafTools.js'
-import PathTools from '../middleware/pathTools.js'
-import helperFunctions from '../middleware/helperFunctions';
-import CookieHelpers from '../middleware/cookieHelper';
+import D3Network from "vue-d3-network";
+import grafhelpers from "../middleware/helperFunctions";
+import Toolbar from "../components/Toolbar.vue";
+import GrafTools from "../middleware/grafTools.js";
+import PathTools from "../middleware/pathTools.js";
+import helperFunctions from "../middleware/helperFunctions";
+import CookieHelpers from "../middleware/cookieHelper";
 //import Help from "../components/Help.vue";
 //import About from 'About.vue'
 
-
 export default {
-  name: 'Graf',
+  name: "Graf",
   components: {
     D3Network,
-    Toolbar
+    Toolbar,
   },
-  mounted () {
-			document.addEventListener("keyup", this.keyup_handler, false);
-      window.addEventListener('resize', this.resize_handler, false);
-      //Loading in the graf from cookies
-      CookieHelpers.checkRepCookie();
-      if(!CookieHelpers.isC) {
-        var d = grafhelpers.loadGraf(CookieHelpers.getCookie("GrafData"));
-        this.graf = d;
-      }
+  mounted() {
+    document.addEventListener("keyup", this.keyup_handler, false);
+    window.addEventListener("resize", this.resize_handler, false);
+    //Loading in the graf from cookies
+    CookieHelpers.checkRepCookie();
+    if (!CookieHelpers.isC) {
+      var d = grafhelpers.loadGraf(CookieHelpers.getCookie("GrafData"));
+      this.graf = d;
+    }
   },
-  data () {
+  data() {
     return {
-        currentTool: "",
-        grafData: "",
-        history: {
-            previous: [],
-            next: []
+      currentTool: "",
+      grafData: "",
+      visible: false,
+      history: {
+        previous: [],
+        next: [],
+      },
+      selection: {
+        selectedAlgorithm: null,
+        selectedCurrent: null,
+        selectedLast: null,
+        selectedLabel: null,
+        selectMultiple: true,
+        selectedNodes: new Set(),
+        selectedEdges: new Set(),
+      },
+      graf: {
+        nodes: [{ id: 0 }],
+        links: [],
+        aggCount: 1,
+      },
+      options: {
+        force: 3000,
+        size: {
+          w: Math.max(1020, window.innerWidth - 200),
+          h: Math.max(720, window.innerHeight - 210),
         },
-        selection: {
-          selectedAlgorithm: null,
-          selectedCurrent: null,
-          selectedLast: null,
-          selectedLabel: null,
-          selectMultiple: true,
-          selectedNodes: new Set(),
-          selectedEdges: new Set()
-        },
-        graf: {
-          nodes: [{ id: 0 }],
-          links: [],
-          aggCount: 1,
-        },
-        options: {
-            force: 3000,
-            size:{ w: Math.max(1020,window.innerWidth - 200), h: Math.max(720,window.innerHeight - 210)},
-            resizeListener: true,
-            nodeSize: 20,
-            nodeLabels: true,
-            linkLabels:true,
-            canvas: false,
-            linkWidth: 3,
-            fontSize: 15
-        }
+        resizeListener: true,
+        nodeSize: 20,
+        nodeLabels: true,
+        linkLabels: true,
+        canvas: false,
+        linkWidth: 3,
+        fontSize: 15,
+      },
     };
   },
   methods: {
     onSaveImage() {
-        grafhelpers.screenshotGraf(document.getElementsByClassName("net-svg")[0]);
+      grafhelpers.screenshotGraf(document.getElementsByClassName("net-svg")[0]);
     },
     onSaveGraf() {
-        grafhelpers.saveGraf(this.graf);
+      grafhelpers.saveGraf(this.graf);
     },
     onLoadGraf() {
-        var data = grafhelpers.loadGraf(this.grafData);
-        this.graf = data;
-        this.grafData = "";
+      var data = grafhelpers.loadGraf(this.grafData);
+      this.graf = data;
+      this.grafData = "";
     },
     onResetGraf() {
-        this.graf.nodes = [{ id: 0 }];
-        this.graf.links = [];
-        this.grafData = "";
-        this.graf.aggCount = 1;
+      this.graf.nodes = [{ id: 0 }];
+      this.graf.links = [];
+      this.grafData = "";
+      this.graf.aggCount = 1;
       //Modifying cookie
       CookieHelpers.putCookie("GrafData", JSON.stringify(this.graf));
     },
     onAlgorithmChange(alg) {
-        this.selection.selectedAlgorithm = alg;
+      this.selection.selectedAlgorithm = alg;
     },
     onUndo() {
-        this.graf = helperFunctions.updateHistory(this.graf, this.history, true);
-        //Modifying cookie
-        CookieHelpers.putCookie("GrafData", JSON.stringify(this.graf));
+      this.graf = helperFunctions.updateHistory(this.graf, this.history, true);
+      //Modifying cookie
+      CookieHelpers.putCookie("GrafData", JSON.stringify(this.graf));
     },
     onRedo() {
-        this.graf = helperFunctions.updateHistory(this.graf, this.history, false);
-        //Modifying cookie
-        CookieHelpers.putCookie("GrafData", JSON.stringify(this.graf));
+      this.graf = helperFunctions.updateHistory(this.graf, this.history, false);
+      //Modifying cookie
+      CookieHelpers.putCookie("GrafData", JSON.stringify(this.graf));
+    },
+    toggleSettings() {
+      this.visible = false;
     },
     useTool(tool) {
       var oldData = JSON.stringify(this.graf);
 
-      switch(tool){
+      switch (tool) {
         case "Select":
           break;
         case "Node":
@@ -176,79 +202,76 @@ export default {
       CookieHelpers.putCookie("GrafData", JSON.stringify(this.graf));
 
       var newData = JSON.stringify(this.graf);
-      if(oldData != newData) {
+      if (oldData != newData) {
         this.history.previous.unshift(oldData);
         this.history.next = [];
       }
     },
-    handle_node_click(event,node) {
-        this.selection.selectedLast = this.selection.selectedCurrent;
-        this.selection.selectedCurrent = node;
-        this.selection.selectedLabel = node;
-        GrafTools.update_selection(this.graf, node, 'node', this.selection);
-        this.useTool(this.currentTool);
+    handle_node_click(event, node) {
+      this.selection.selectedLast = this.selection.selectedCurrent;
+      this.selection.selectedCurrent = node;
+      this.selection.selectedLabel = node;
+      GrafTools.update_selection(this.graf, node, "node", this.selection);
+      this.useTool(this.currentTool);
     },
-    handle_edge_click(event,edge) {
-        this.selection.selectedLabel = edge;
-        GrafTools.update_selection(this.graf, edge, 'edge', this.selection);
-        this.useTool(this.currentTool);
+    handle_edge_click(event, edge) {
+      this.selection.selectedLabel = edge;
+      GrafTools.update_selection(this.graf, edge, "edge", this.selection);
+      this.useTool(this.currentTool);
     },
-    change_tool (tool) {
-        if(tool == 'Select') this.selection.selectMultiple = true;
-        else this.selection.selectMultiple = false;
-        this.selection.selectedCurrent = null;
-        this.selection.selectedLast = null;
-        this.selection.selectedLabel = null;
-        this.currentTool = tool;
-        this.useTool(tool);
+    change_tool(tool) {
+      if (tool == "Select") this.selection.selectMultiple = true;
+      else this.selection.selectMultiple = false;
+      this.selection.selectedCurrent = null;
+      this.selection.selectedLast = null;
+      this.selection.selectedLabel = null;
+      this.currentTool = tool;
+      this.useTool(tool);
     },
     keyup_handler(event) {
-      if(event.code == "Escape") {
+      if (event.code == "Escape") {
         PathTools.update_distances(this.graf, null, false);
-        GrafTools.clear_selection(this.graf, this.selection)
+        GrafTools.clear_selection(this.graf, this.selection);
       }
-      if(event.ctrlKey && event.code === 'KeyZ')
-        this.onUndo();
-      if(event.ctrlKey && event.code === 'KeyY')
-        this.onRedo();
+      if (event.ctrlKey && event.code === "KeyZ") this.onUndo();
+      if (event.ctrlKey && event.code === "KeyY") this.onRedo();
     },
     resize_handler() {
-      this.options.size.w = Math.max(1020,window.innerWidth - 200);
-      this.options.size.h = Math.max(720,window.innerHeight - 210);
+      this.options.size.w = Math.max(1020, window.innerWidth - 200);
+      this.options.size.h = Math.max(720, window.innerHeight - 210);
       // Hacky BS to force update of the d3 network, should fork and workaround
-      this.graf.nodes.push({id: -1});
-      this.graf.nodes.splice(this.graf.nodes.length-1,1)
-    }
-  }
-}
-
+      this.graf.nodes.push({ id: -1 });
+      this.graf.nodes.splice(this.graf.nodes.length - 1, 1);
+    },
+  },
+};
 </script>
 
 <style scoped>
-.fixedTR{
-  position:fixed;
-  top:0;
+.fixedTR {
+  position: fixed;
+  top: 0;
   right: 0;
 }
-.fixedBR{
-  position:fixed;
+.fixedBR {
+  position: fixed;
   bottom: 0;
   right: 0;
 }
-.fixedTC{
-  position:fixed;
+.fixedTC {
+  position: fixed;
   width: 800px;
   height: 80px;
-  top:0;
-  right:50%;
+  top: 0;
+  right: 50%;
   margin-right: -400px;
 }
-.fixedBC{
-  position:fixed;
+.fixedBC {
+  position: fixed;
   width: 360px;
   height: 80px;
-  bottom:0;
-  right:50%;
+  bottom: 0;
+  right: 50%;
   margin-right: -180px;
   margin-bottom: auto;
 }
