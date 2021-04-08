528cbaaca751bde29c12014baa6abc142fc55c42
maxstewartsteele <66533762+maxstewartsteele@users.noreply.github.com>
2021-04-07 21:09:38 -0400

    Update main.js

--- Vue/graf/src/main.js
+++ Vue/graf/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import SuiVue from 'semantic-ui-vue';
 import router from './router';
+// import FomanticUI from 'vue-fomantic-ui'
+// import 'fomantic-ui-css/semantic.min.css'
 
 Vue.config.productionTip = false
 
 Vue.use(SuiVue);
+// Vue.use(FomanticUI);
 
 new Vue({
   router,
