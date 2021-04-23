c0049908f306a67ccdeb893629a85bc1caf60978
maxstewartsteele <66533762+maxstewartsteele@users.noreply.github.com>
2021-04-23 16:18:03 -0400

    Update Settings.vue

--- Vue/graf/src/components/Settings.vue
+++ Vue/graf/src/components/Settings.vue
@@ -13,7 +13,7 @@
 			>
 				<sui-menu-item>
 					<div class="slidecontainer">
-  						<input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+					<input type="range" min="1" max="100" value="50" class="slider" id="myRange">
 					</div>
 				<sui-menu-item to="/">
 					<sui-icon name="home" />
