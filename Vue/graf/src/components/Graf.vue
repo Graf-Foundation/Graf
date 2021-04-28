fa54182dfd416cde8c0607ad77734c46c97f614a
maxstewartsteele <66533762+maxstewartsteele@users.noreply.github.com>
2021-04-28 17:34:57 -0400

    Update Graf.vue

--- Vue/graf/src/components/Graf.vue
+++ Vue/graf/src/components/Graf.vue
@@ -188,14 +188,7 @@ export default {
     onSliderChange(val, need) {
       if(need == 1) {this.options.nodeSize = val}
       if(need == 2) {this.options.force = (1/val)*10000}
-      if(need == 3) {
-        this.options.linkWidth = val
-        console.log(document.cookie)
-        // console.log(this.graf.links.length)
-        // for(var i = 0; i < this.graf.links.length; i++) {
-        //   console.log(this.graf.links[i])
-        // }
-      }
+      if(need == 3) {this.options.linkWidth = val}
       this.options = Object.assign({},this.options)
       
     },
