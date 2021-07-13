9cb392e723cf9cb1726f5754d54923f8c4df5d78
maxstewartsteele <66533762+maxstewartsteele@users.noreply.github.com>
2021-07-13 16:01:22 -0400

    Update InfoBox.vue
    fixing UI issues allows for naming nodes from infobox and deleteing and deselcting edges from info box

--- Vue/graf/src/components/InfoBox.vue
+++ Vue/graf/src/components/InfoBox.vue
@@ -11,27 +11,40 @@
       </sui-card-content>
       <sui-card-content v-for="n in selected.selectedNodes" v-bind:key="n.id">
         <h5>
-          <sui-button
-          @click ="onDeselectNode(n)" 
-          circular 
-          class = "right floated"
-          size = "mini"
-          icon="angle down" />
-
           <sui-button 
           @click ="onDeleteNode(n)" 
           circular 
           class = "right floated"
           size = "mini" 
           icon="trash alternate outline"/>
-          Name: 
+
+          <sui-button
+          @click ="onDeselectNode(n)" 
+          circular 
+          class = "right floated"
+          size = "mini"
+          icon="angle down" />
         </h5> 
-        {{ n.name }}
+        <div class = "smallerv2"> Name: <sui-input fluid v-model ="n.name"  @keypress.stop/> </div>
       </sui-card-content>
       <sui-card-content v-for="e in selected.selectedEdges" v-bind:key="e.id">
-        <h4> Weight:
-          <sui-input fluid v-model ="e.name"  @keypress.stop/>
-        </h4>
+        <h5>
+          <sui-button 
+          @click ="onDeleteEdge(e)" 
+          circular 
+          class = "right floated"
+          size = "mini" 
+          icon="trash alternate outline"/> 
+
+          <sui-button
+          @click ="onDeselectEdge(e)" 
+          circular 
+          class = "right floated"
+          size = "mini"
+          icon="angle down" />
+          </h5>
+        <div class = "smaller"> Weight: <sui-input fluid v-model ="e.name"  @keypress.stop/> </div>
+        
         <h5>Type: </h5>
         {{ e.type }}
       </sui-card-content>
@@ -93,8 +106,13 @@ export default {
     },
     onDeleteNode(n) { 
       this.$emit('del-node', n);
-
     },
+    onDeselectEdge(e) {
+      this.$emit('des-edge', e);
+    },
+    onDeleteEdge(e) { 
+      this.$emit('del-edge', e);
+    }
 
   },
   props: ['selected']
@@ -113,6 +131,22 @@ h4 {
   width: 35%;
   
 }
+.smaller {
+  display: block;
+  font-size: 10.5pt;
+  font-weight: bold;
+  margin-bottom: 0;
+  width: 35%;
+}
+
+.smallerv2 {
+  display: block;
+  font-size: 10.5pt;
+  font-weight: bold;
+  margin-bottom: 0;
+  width: 55%;
+}
+
 .infoBox {
   position: absolute;
   z-index: 9;
