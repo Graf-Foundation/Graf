9867c074ae2895e05d3345eaa9394ea9428c9878
maxstewartsteele <66533762+maxstewartsteele@users.noreply.github.com>
2021-07-18 18:56:32 -0400

    Create Load.vue

---
+++ Vue/graf/src/components/Load.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <sui-modal v-model='open'>
+      <sui-modal-header>Welcome to Graf!</sui-modal-header>
+      <sui-modal-content scrolling>
+        <sui-header>Quick Start Guide</sui-header>
+        <div v-html="helpContent"></div>
+      </sui-modal-content>
+    </sui-modal>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Load",
+  data () {
+    return {
+      open: false,
+      helpContent: "<p>Something went wrong</p>"
+    }
+  },
+  methods: {
+    toggle() {
+      this.open = !this.open
+    }
+  },
+  created: function () {
+    this.$root.$on('openLoad', () => {
+      this.toggle();
+    });
+    this.$http.get('static/html/Help.html').then(function(response){
+      this.helpContent = response.data;
+    });
+  }
+}
+
+</script>
+
+
+
+
