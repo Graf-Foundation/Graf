49c3bbcce442a720266277dab867ef7ab7909398
maxstewartsteele <66533762+maxstewartsteele@users.noreply.github.com>
2021-07-22 20:52:17 -0400

    Update Graf.vue

--- Vue/graf/src/components/Graf.vue
+++ Vue/graf/src/components/Graf.vue
@@ -8,7 +8,7 @@
         <sui-button @click="onUndo();" icon="undo" data-tooltip="Ctrl-Z" data-position="bottom center"/>
         <sui-button @click="onRedo();" icon="redo" data-tooltip="Ctrl-Y" data-position="bottom center"/>
         <sui-button @click="clear_selections()" icon ="eye slash" data-tooltip="Esc" data-position="bottom center"/>
-        <sui-button @click="info()" icon ="button" data-position="bottom center"/>
+        <!-- <sui-button @click="info()" icon ="button" data-position="bottom center"/> -->
         <InfoBox v-if="selection.selectedNodes.size || selection.selectedEdges.size" v-bind:selected="selection" 
         @del-node="onInfoNodeDel"
         @des-node="onInfoNodeDes"
