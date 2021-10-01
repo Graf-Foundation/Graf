4bc698c11954efcb9758a1da8e3f2a358b0d6846
Michael Fitzgerald <fitzichael@gmail.com>
2021-10-01 17:07:25 -0400

    added some settings options

--- Vue/graf/src/components/ui_components/GrafSettings.vue
+++ Vue/graf/src/components/ui_components/GrafSettings.vue
@@ -3,7 +3,30 @@
     <v-card>
       <v-card-title>Settings</v-card-title>
       <v-card-text>
-        These are some settings. WIP.
+        <v-list>
+          <v-list-item>
+            <v-list-item-action>
+              <v-select :items="['Light', 'Dark']" label="Theme" v-bind:value="theme"></v-select>
+            </v-list-item-action>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-action>
+              <v-checkbox v-model="grafDirected"></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Directionality</v-list-item-title>
+              <v-list-item-subtitle>Enable visualization of arrows on edges</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-slider v-model="grafForce"
+                        label="Force of the graph"
+                        min="0"
+                        max="100"></v-slider>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
       </v-card-text>
       <v-divider></v-divider>
       <v-card-actions>
@@ -22,6 +45,11 @@ export default {
   methods: {
     emit(eventString) {
       this.$emit(eventString);
+    }
+  },
+  data: function () {
+    return {
+      theme: "Light"
     }
   }
 }
